355614c048656191a540dea053f3061297e41875
SB - Number <spicer.bak.frf@gmail.com>
2022-04-28 14:03:40 +0000

    updated to pull survey number if key in netCDF file

--- getDataFRF.py
+++ getDataFRF.py
@@ -2844,7 +2844,7 @@ class getDataTestBed:
                     'lat':       lat,
                     'lon':       lon, }
         # then its a survey, get the survey number
-        if ('cBKF_T' not in kwargs) and ('cBKF' not in kwargs):
+        if 'surveyNumber' in self.ncfile.variables.keys():
             gridDict['surveyNumber'] = self.ncfile['surveyNumber'][idx]
         
         return gridDict
