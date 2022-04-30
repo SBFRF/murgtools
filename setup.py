2b122d58ebefe0c76db41aeab3a4d1bb86584abb
spike <spicer.bak.frf@gmail.com>
2022-04-30 12:01:22 -0400

    minor updates to requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 """Setup tools for getDataTestBed."""
 from setuptools import setup
 
-setup(name='GetDataTestbed',
-      version='0.1.dev',
+setup(name='getdatatestbed',
+      version='0.9.dev',
       description=('Utilities for retrieving data relevant to the USACE' +
                    'Field Research Facility Coastal Model Test Bed' +
                    '(CMTB)'),
       author='Spicer Bak',
+      author_email='spicer.bak@usace.army.mil',
+      packages='getdatatestbed',
       modules=['getDataFRF', 'getOutsideData'],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
      )
